158c5bf0fed5d04ae53a3831a672a49ff1669a5d
kalm-dev <ixperimentals@gmail.com>
2024-04-15 07:33:43 +0530

    second

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://vid-chat-client-3s282g9cw-kalm-devs-projects.vercel.app/",
+		origin: "https://vid-chat-r2-client.vercel.app/",
 		methods: [ "GET", "POST" ]
 	}
 })
